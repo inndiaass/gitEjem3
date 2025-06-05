b90684398697bf1426b51833898e3311e52c4736
inndiaass <indiascr7@gmail.com>
2025-06-05 13:48:21 +0200

    ver 2

--- src/Departamento.java
+++ src/Departamento.java
@@ -1,7 +1,7 @@
 
 public class Departamento {
-    private String nombre;
-    private int numEmpleados;
+    private String Nombre;
+    private int NumEmpleados;
     private Empleado[] empleados = new Empleado[2];
     void anadir(Empleado v){
         empleados[0]=v;
